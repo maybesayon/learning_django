d2ec2ca2589fe39f61ca1edb292e8bf785e219dd
maybesayon <sayoncoldplay@gmail.com>
2023-09-11 08:10:43 +0545

    templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import home
 from .views import school
+from .views import test
+from .views import test1
+
 
 urlpatterns =[
-     path("world/", home),
-     path("python/", school)
+     path("", home),
+     path("python/", school),
+     path("test/", test),
+     path("test1/", test1)
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -3,11 +3,18 @@ from django.http import HttpResponse
 
 
 def home(request):
-    content = """
-<h1>Hello World</h1>
-<h2>I'm learning python</h2>
-"""
-    return HttpResponse(content)
+    return render(request, template_name="myapp/test.html")
 
 def school(request):
     return HttpResponse("I'm learning Python")
+
+def test(request):
+    # We can send quarry strings / query parameters in the urls
+    # Everything sent after "?" in the url is quarystring
+    # Query strings can be multiple and are separated by ampersand(&.)
+    name = request.GET.get("name")
+
+    return HttpResponse (f"<h1> My name is {name}</h1>")
+
+def test1(request):
+    return render(request, template_name="myapp/test.html")
