c23f64728c22462f74789adbfe5f789239d89f99
maybesayon <sayoncoldplay@gmail.com>
2023-09-13 09:34:15 +0545

    static

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,12 +3,14 @@ from .views import home
 from .views import school
 from .views import test
 from .views import test1
+from .views import portfolio
 
 
 urlpatterns =[ 
      path("python/", school),
      path("test/", test),
-     path("test1/", test1),
+     path("yay/", test),
+     path("portfolio/", portfolio),
      path("", test1)
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -9,12 +9,7 @@ def school(request):
     return HttpResponse("I'm learning Python")
 
 def test(request):
-    # We can send quarry strings / query parameters in the urls
-    # Everything sent after "?" in the url is quarystring
-    # Query strings can be multiple and are separated by ampersand(&.)
-    name = request.GET.get("name")
-
-    return HttpResponse (f"<h1> My name is {name}</h1>")
+    return render(request, template_name="myapp/test1.html")
 
 def test1(request):
     peoples_info= [
@@ -26,3 +21,6 @@ def test1(request):
     table_heading = "People Information"
     return render(request, template_name="myapp/test.html",
                    context={"heading": table_heading, "infos": peoples_info})
+
+def portfolio(request):
+    return render(request, template_name="myapp/index.html")
