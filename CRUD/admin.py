b9c93aa54fe03d5a40c2f8750bb0201ac2af5be3
maybesayon <sayoncoldplay@gmail.com>
2023-09-29 09:32:06 +0545

    new stuff

--- CRUD/admin.py
+++ CRUD/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ClassRoom
+from .models import ClassRoom, Student, StudentProfile
 admin.site.register(ClassRoom)
+admin.site.register(Student)
+admin.site.register(StudentProfile)
 
 
 
--- CRUD/views.py
+++ CRUD/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import StudentProfile, Student, ClassRoom
+from .models import Student, ClassRoom, StudentProfile
 
 def crud_classroom(request):
     if request.method == "POST":
@@ -28,5 +28,19 @@ def classroom_update(request, id):
     
     return render(request, template_name="CRUD/classroom_update.html", context={"classroom":classroom})
 
+def crud_student(request):
+    students= Student.objects.all()
+    return render(request, template_name='CRUD/student.html', context={"students":students})
 
+def add_student(request):
+    if request.method=="POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        age = request.POST.get("age")
+        address = request.POST.get("address")
+        contact = request.POST.get("contact")
+        student=Student.objects.create(name=name, email=email, age=age, classroom_id=1)
+        StudentProfile.objects.create(address=address, contact=contact, student=student)
+        return redirect("crud_student")
+    return render(request, template_name="CRUD/add_student.html", context={"title":"Add Student"})
 
