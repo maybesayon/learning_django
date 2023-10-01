bf4c5f665a1f0ca7b919d314650bef482f3c5b22
maybesayon <sayoncoldplay@gmail.com>
2023-10-01 09:30:26 +0545

    updated

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import crud_classroom, classroom_delete, classroom_update, crud_student, add_student
+from .views import crud_classroom, classroom_delete, classroom_update, crud_student, add_student, delete_student, student_detail
 urlpatterns = [
     path("classroom/delete/<int:id>/", classroom_delete, name = "classroom_delete"),
     path("classroom/update/<int:id>/", classroom_update, name="classroom_update"),
     path("student/", crud_student, name="crud_student"),
     path("add-student/", add_student, name="add_student"), 
+    path("delete-student/<int:id>", delete_student, name="delete_student"),
+    path("student/<int:id>", student_detail, name="student_detail"),
     path("", crud_classroom, name = "crud_classroom")
 ]
--- CRUD/views.py
+++ CRUD/views.py
@@ -39,8 +39,22 @@ def add_student(request):
         age = request.POST.get("age")
         address = request.POST.get("address")
         contact = request.POST.get("contact")
+        pp = request.FILES.get("pp")
         student=Student.objects.create(name=name, email=email, age=age, classroom_id=1)
-        StudentProfile.objects.create(address=address, contact=contact, student=student)
+        sp = StudentProfile.objects.create(address=address, contact=contact, student=student)
+        if pp:
+            sp.profile_picture=pp
+            sp.save()
         return redirect("crud_student")
     return render(request, template_name="CRUD/add_student.html", context={"title":"Add Student"})
 
+def delete_student(request, id):
+    student = Student.objects.get(id=id)
+    if request.method == "POST":
+        student.delete()
+        return redirect(crud_student)
+    return render(request, template_name="CRUD/delete_student.html", context={"student":student})
+
+def student_detail(request, id):
+    student = Student.objects.get(id=id)
+    return render(request, template_name="CRUD/student_detail.html", context={"title":"Student detail", "student": student})
