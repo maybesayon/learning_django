4c5c263de44b59c8b21ed06a1bf2a2bb36bdffeb
maybesayon <sayoncoldplay@gmail.com>
2023-10-08 09:32:24 +0545

    api

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import hello_world, HelloWorldView, Student
+from .views import hello_world, HelloWorldView, StudentView, StudentFromDBView, StudentFromDBListView, ProfileFromDBListView
 urlpatterns = [
     path("hello-world/", hello_world),
     path("message/", HelloWorldView.as_view()),
-    path("student/",Student.as_view() )
+    path("student/",StudentView.as_view()),
+    path("student-from-db/<int:id>/", StudentFromDBView.as_view()),
+    path("student-from-db/", StudentFromDBListView.as_view()),
+    path("profile-from-db/", ProfileFromDBListView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from CRUD.models import Student, StudentProfile
 def hello_world(request):
     respose = {
-        "message": "Hello World. I'm learning API"
+        "message": "Hello World. I'm learning API."
     }
 
     return JsonResponse(respose)
@@ -18,7 +19,7 @@ class HelloWorldView(APIView):
 
         return Response(response)
     
-class Student(APIView):
+class StudentView(APIView):
     def get (self, *args, **kwargs):
         student =[
             {"name": "Jacob", "age": "40", "address": "TX"},
@@ -30,3 +31,62 @@ class Student(APIView):
         return Response(student)
 
 
+class StudentFromDBView(APIView):
+    def get(self, *args, **kwargs):
+        id = kwargs.get("id")
+        try:
+            student = Student.objects.get(id=id)
+        except Student.DoesNotExist:
+            return Response({
+                "detail": "Invalid Student ID"
+            })
+        response={
+            "name": student.name,
+            "age": student.age,
+            "email": student.email,
+            "classroom": student.classroom.name 
+            }
+        
+        return Response(response)
+    
+    
+
+class StudentFromDBListView(APIView):
+    def get(self, *args, **kwargs):
+        students = Student.objects.all()
+        response=[]
+        for student in students:
+            data={
+            "name": student.name,
+            "age": student.age,
+            "email": student.email,
+            "classroom": student.classroom.name 
+            }
+            response.append(data)
+        return Response(response)
+    def post(self, *args, **kwargs):
+         #name = request.POST.get("name")
+        print(self.request.data)
+        name = self.request.data.get("name")
+        email = self.request.data.get("email")
+        age = self.request.data.get("age")
+        classroom = self.request.data.get("classroom")
+        Student.objects.create(name=name, email=email, age=age, classroom_id =classroom)
+        return Response({
+            "detail": "Student added sucessfully"
+        })
+
+    
+class ProfileFromDBListView(APIView):
+    def get(self, *args, **kwargs):
+        profile = StudentProfile.objects.all()
+        response=[]
+        for data in profile:
+            data={
+               "student": data.student.name,
+               "address": data.address,
+               "contact": data.contact,
+               "pp": data.profile_picture.url
+           }
+            response.append(data)
+        return Response(response)
